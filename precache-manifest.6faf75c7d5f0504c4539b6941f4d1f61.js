380b45ba54f247267def50f58a180a5cfbc1e93b
salwks <stra2003@gmail.com>
2019-10-28 17:25:20 +0900

    Updates

---
+++ precache-manifest.6faf75c7d5f0504c4539b6941f4d1f61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e313782f46b7141e2fd22ca8e8015f",
+    "url": "/Movie_review_2019/index.html"
+  },
+  {
+    "revision": "87c8087a7935b078a103",
+    "url": "/Movie_review_2019/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "d86d88e726660e8b55a5",
+    "url": "/Movie_review_2019/static/js/2.16276eec.chunk.js"
+  },
+  {
+    "revision": "87c8087a7935b078a103",
+    "url": "/Movie_review_2019/static/js/main.fb6dab11.chunk.js"
+  },
+  {
+    "revision": "f62f655dbf27a3e7cdb1",
+    "url": "/Movie_review_2019/static/js/runtime-main.4eefd47a.js"
+  }
+]);
